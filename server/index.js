541e837da62fa5aec060beb1d65273e8ade28912
anhye0n <anhye0ndev@gmail.com>
2021-05-16 08:58:21 +0900

    main 수정

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const conn = db_info.init()
 const fs = require('fs')
 
 app.get('/', function (req, res){
-    fs.readFile('../views/index/html', function (err, data){
+    fs.readFile('../views/index.html', function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.end(data)
     })
