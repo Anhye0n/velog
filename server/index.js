ea4c285b28eb253a27ffbdc193b2a7795f31be47
anhye0n <anhye0ndev@gmail.com>
2021-05-16 08:57:27 +0900

    main 수정

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express()
 const port = 80
 const db_info = require('../conf/db_info')
 const conn = db_info.init()
+const fs = require('fs')
 
-var router = require('./routes/main')(app);
-
-
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.get('/', function (req, res){
+    fs.readFile('../views/index/html', function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(data)
+    })
+})
 
 
 
