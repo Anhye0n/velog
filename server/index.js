710b68dd53317fe4bc8176ae842eb8e8850fa16e
anhye0n <anhye0ndev@gmail.com>
2021-05-17 02:14:15 +0900

    수정

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({extend:false}))
 
 app.use(bodyParser.json())
 
-// app.set('views', serveStatic(path.join(__dirname, '../views'))) //메인 주소
-
-app.use('/', express.static(__dirname + '../views'))
+app.set('views', serveStatic(path.join(__dirname, '../views'))) //메인 주소
+//
+// app.use('/', express.static(__dirname + '../views'))
 // app.use('/user/register', regitser)
 // app.use('/user/login', express.static('../views'))
 
