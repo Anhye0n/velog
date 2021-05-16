afefef2fe8fd8bdf86f4f750d51ca00e0f3947b0
anhye0n <anhye0ndev@gmail.com>
2021-05-16 20:22:37 +0900

    passport 다운로드

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 const express = require('express')
 const app = express()
 
+//DB
 const db_info = require('../conf/db_info')
 const conn = db_info.init()
 
+//모듈
 const fs = require('fs')
 const bodyParser = require('body-parser'), serveStatic = require('serve-static'), path = require('path')
+const passport = require('passport')
+
+//라우터
+const user_info = require('./routes/main')
 
 app.use(bodyParser.urlencoded({extend:false}))
 
 app.use(bodyParser.json())
 
-app.use('/', serveStatic(path.join(__dirname, '../views')))
+views_url = serveStatic(path.join(__dirname, '../views'))
+
+app.use('/', views_url)
 
 
 app.get('/list', function (req, res) {
-    var sql = 'INSERT INTO board (id, title, content) VALUES (\'Company Inc\', \'Highway 37\', \'asdf\')';
 
-    conn.query(sql, function (err, result) {
-        if(err) console.log('query is not excuted. select fail...\n' + err);
-        console.log('Success Insert!'+ result)
-    });
 });
 
 app.use(express.static('src/css'))
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,13 @@
-module.exports = function (app){
-    app.get('/',function(req,res){
-        res.render('index.html')
+var express = require('express');
+var router = express.Router();
+
+router.get('/api/register', function(req, res, next) {
+    var sql = 'INSERT INTO user_info (name, email, id, password) VALUES (\'Company Inc\', \'Highway 37\', \'asdf\')';
+
+    conn.query(sql, function (err, result) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        console.log('Success Insert!'+ result)
     });
-}
+});
+
+module.exports = router;
