207a5f593c6b9bc2f94d35a9096b25d93545caa7
anhye0n <anhye0ndev@gmail.com>
2021-05-23 16:50:04 +0900

    index.js 수정

--- backend/server/index.js
+++ backend/server/index.js
@@ -13,17 +13,17 @@ app.use(bodyParser.urlencoded({extend:false}))
 
 app.use(bodyParser.json())
 
-app.set('view engine', 'ejs') //ejs 사용
-app.set('views', path.join(__dirname, '../views/'))
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
+// app.set('view engine', 'ejs') //ejs 사용
+// app.set('views', path.join(__dirname, '../views/'))
 //
-// app.use('/', serveStatic(path.join(__dirname, '../views'))) //메인 주소
-//
-// app.use('/user', serveStatic(path.join(__dirname, '../views/user'))) //유저 관리 주소
+// app.get('/', (req, res) => {
+//     res.render('index');
+// })
+
+
+app.use('/', serveStatic(path.join(__dirname, '../views'))) //메인 주소
+
+app.use('/user', serveStatic(path.join(__dirname, '../views/user'))) //유저 관리 주소
 
 //라우터
 const user_info = require('./routes/user')
