6448384a0d7f83ab5db351cfe4f3cae9a1cbd39c
anhye0n <anhye0ndev@gmail.com>
2021-05-23 12:34:23 +0900

    ejs 링크 수정

--- server/index.js
+++ server/index.js
@@ -8,13 +8,14 @@ const app = express()
 //모듈
 const bodyParser = require('body-parser'), serveStatic = require('serve-static'), path = require('path')
 const passport = require('passport')
+var ejs = require('ejs')
 
 app.use(bodyParser.urlencoded({extend:false}))
 
 app.use(bodyParser.json())
 
 app.set('view engine', ejs) //ejs 사용
-app.set('views', path.join(__dirname, '../views/'))
+app.set('views', serveStatic(path.join(__dirname, '../views/')))
 
 app.get('/', (req, res) => {
     res.render('index');
