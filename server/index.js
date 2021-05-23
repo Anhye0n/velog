ee8766514fbab49110c97599c5db87183eb2f7ad
anhye0n <anhye0ndev@gmail.com>
2021-05-23 12:05:59 +0900

    set을 use로 수정

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(bodyParser.json())
 
 app.set('view engine', ejs) //ejs 사용
 
-app.set('/', serveStatic(path.join(__dirname, '../views'))) //메인 주소
+app.use('/', serveStatic(path.join(__dirname, '../views'))) //메인 주소
 
-app.set('/user', serveStatic(path.join(__dirname, '../views/user'))) //유저 관리 주소
+app.use('/user', serveStatic(path.join(__dirname, '../views/user'))) //유저 관리 주소
 
 //라우터
 const user_info = require('./routes/user')
