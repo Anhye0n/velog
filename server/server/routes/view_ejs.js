8b95a465e8c50fecd567d9130a7a63c7251f1c90
anhye0n <anhye0ndev@gmail.com>
2021-06-07 04:17:48 +0900

    adf

--- server/server/routes/view_ejs.js
+++ server/server/routes/view_ejs.js
@@ -15,8 +15,6 @@ router.get('/user/register', (req, res) => {
     res.render('./user/register');
 })
 router.get('/user/login', (req, res) => {
-    let fmsg = req.flash()
-    console.log(fmsg)
     res.render('./user/login');
 })
 router.get('/user/login_success', (req, res) => {
