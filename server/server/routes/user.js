fab270ccffa25e6b02a9b941fa61dfb1741d3f9d
anhye0n <anhye0ndev@gmail.com>
2021-06-07 01:35:51 +0900

    session 부분 수정

--- server/server/routes/user.js
+++ server/server/routes/user.js
@@ -14,13 +14,13 @@ const mysqlStore = require('express-mysql-session')(session)
 
 //passport
 const passport = require('passport')
-const LocalStrategy = require('passport-local')
+const LocalStrategy = require('passport-local').Strategy
 const flash = require('connect-flash')
 
 router.use(session({
     secret: 'session key',
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     store: new mysqlStore(db_info.db_info)
 }))
 
@@ -86,60 +86,65 @@ passport.deserializeUser(function (id, done) {
 });
 
 passport.use('local-login', new LocalStrategy({
-        // Form에서 post로 받아온 값임.
-        // 기본값은 username, password이지만 이름이 다르게 설정되있으면 여기서 설정
-        usernameField: 'id',
-        passwordField: 'password'
-    }, function (username, password, done) {
-        let sql = "SELECT exists (SELECT * FROM user_info WHERE id=?) as success;"
+    // Form에서 post로 받아온 값임.
+    // 기본값은 username, password이지만 이름이 다르게 설정되있으면 여기서 설정
+    usernameField: 'id',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function (username, password, done) {
+    let sql = "SELECT exists (SELECT * FROM user_info WHERE id=?) as success;"
 
-        conn.query(sql, username, function (err, result) {
-            let id = result[0].success;
-            if (err) {
-                return done(err);
-            }
-            if (id === 0) {
-                return done(null, false, {message: 'Incorrect ID.'});
-            } else if (id === 1) {
-                let in_sql = "SELECT user_salt FROM user_info WHERE id=?;" +
-                    "SELECT password FROM user_info WHERE id=?;" +
-                    "SELECT name FROM user_info WHERE id=?;" +
-                    "SELECT email FROM user_info WHERE id=?;"
+    conn.query(sql, username, function (err, result) {
+        let id = result[0].success;
+        if (err) {
+            return done(err);
+        }
+        if (id === 0) {
+            return done(null, false, {message: 'Incorrect ID.'});
+        } else if (id === 1) {
+            let in_sql = "SELECT user_salt FROM user_info WHERE id=?;" +
+                "SELECT password FROM user_info WHERE id=?;" +
+                "SELECT name FROM user_info WHERE id=?;" +
+                "SELECT email FROM user_info WHERE id=?;"
 
-                conn.query(in_sql, [username, username, username, username], function (err, result) {
-                    let salt = result[0][0].user_salt
-                    let db_password = result[1][0].password
-                    let db_name = result[2][0].name
-                    let db_email = result[3][0].email
+            conn.query(in_sql, [username, username, username, username], function (err, result) {
+                let salt = result[0][0].user_salt
+                let db_password = result[1][0].password
+                let db_name = result[2][0].name
+                let db_email = result[3][0].email
 
-                    crypto.pbkdf2(password, salt, 100, 64, 'sha512', (err, key) => {
-                        let de_password = key.toString("base64")
-                        // 비밀번호 맞을 때
-                        if (de_password === db_password) {
-                            // req.session.user_id = username
-                            // req.session.save()
-                            const user = {
-                                id: username,
-                                password: de_password,
-                                name: db_name,
-                                email: db_email
-                            }
-                            return done(null, user);
-                        } else { // 비밀번호 안 맞을 때
-                            return done(null, false, {message: 'Incorrect Password.'});
+                crypto.pbkdf2(password, salt, 100, 64, 'sha512', (err, key) => {
+                    let de_password = key.toString("base64")
+                    // 비밀번호 맞을 때
+                    if (de_password === db_password) {
+                        // req.session.user_id = username
+                        // req.session.save()
+                        const user = {
+                            id: username,
+                            password: de_password,
+                            name: db_name,
+                            email: db_email
                         }
-                    });
-                })
-            }
-        })
-    }
-));
+                        return done(null, user);
+                    } else { // 비밀번호 안 맞을 때
+                        return done(null, false, {message: 'Incorrect Password.'});
+                    }
+                });
+            })
+        }
+    })
+}));
 
 router.post('/user/login', passport.authenticate('local-login', {
-    successRedirect: '/user/login_success.html',
+    // successRedirect: '/user/login_success.html',
     failureRedirect: '/user/login.html',
     failureFlash: true
-}))
+}), function (req, res){
+    req.session.save(function (){
+        console.log('session save..')
+        res.redirect('/user/login_success.html')
+    })
+})
 
 router.get('/user/logout', function (req, res) {
     req.logout();
