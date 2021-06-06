c1bde0e615b7d3525f79d8dfa6206b3de9101f8e
anhye0n <anhye0ndev@gmail.com>
2021-06-07 04:08:58 +0900

    adf

--- server/server/routes/user.js
+++ server/server/routes/user.js
@@ -28,13 +28,13 @@ router.use(passport.initialize()) //passport를 사용하도록 설정
 router.use(passport.session()) // passport 사용 시 session을 활용
 router.use(flash())
 
-app.get('/flash', function(req, res){
+router.get('/flash', function(req, res){
     // Set a flash message by passing the key, followed by the value, to req.flash().
     req.flash('info', 'Flash is back!')
     res.redirect('/');
 });
 
-app.get('/', function(req, res){
+router.get('/', function(req, res){
     // Get an array of flash messages by passing the key to req.flash()
     res.render('index', { messages: req.flash('info') });
 });
