dff30fd0679beb6ee8a1e4bf308517c7f0f21493
anhye0n <anhye0ndev@gmail.com>
2021-06-10 15:35:20 +0900

    test

--- server/server/routes/view_ejs.js
+++ server/server/routes/view_ejs.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const sess = req.session;
 
 
 router.get('/', (req, res) => {
-    const sess = req.session;
     res.render('./user/index', {
         sess: sess
     });
