a81f6253b5c8f3d96307b13366ef25d70736f790
anhye0n <anhye0ndev@gmail.com>
2021-06-10 15:32:33 +0900

    test

---
+++ server/server/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+
+// DB
+const db_info = require('../conf/db_info')
+const conn = db_info.init()
+
+//모듈
+const bodyParser = require('body-parser'), serveStatic = require('serve-static'), path = require('path')
+const passport = require('passport')
+//session
+const session = require('express-session')
+const mysqlStore = require('express-mysql-session')(session)
+
+app.use(bodyParser.urlencoded({extend:false}))
+
+app.use(bodyParser.json())
+
+//views 라우터
+app.set('views', path.join(__dirname, '../views'))
+app.set('view engine', 'ejs') //ejs 사용
+
+app.use(session({
+    secret: 'session key',
+    resave: false,
+    saveUninitialized: true,
+    store: new mysqlStore(db_info.db_info)
+}))
+
+//views 파일
+const view_router = require('./routes/view_ejs')
+app.use('/', view_router)
+
+//라우터
+const user_info = require('./routes/user')
+app.use('/api', user_info)
+
+const db_test = require('./routes/admin')
+app.use('/admin', db_test)
+
+app.listen(80, () => {
+    console.log(`Example app listening at http://anhye0n.me`)
+})
+
+module.exports = app
--- server/server/routes/view_ejs.js
+++ server/server/routes/view_ejs.js
@@ -1,33 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-//DB
-const db_info = require('../../conf/db_info')
-const conn = db_info.init()
 
-//crypto
-const session = require('express-session')
 
 router.get('/', (req, res) => {
-    res.render('./user/index');
+    const sess = req.session;
+    res.render('./user/index', {
+        sess: sess
+    });
+    console.log(sess)
 })
 router.get('/user/register', (req, res) => {
-    res.render('./user/register');
+    res.render('./user/register', {
+        sess: sess
+    });
 })
 router.get('/user/login', (req, res) => {
-    res.render('./user/login');
+    res.render('./user/login', {
+        sess: sess
+    });
 })
 router.get('/user/login_success', (req, res) => {
-    res.render('./user/login_success');
+    res.render('./user/login_success', {
+        sess: sess
+    });
 })
 router.get('/user/regi_success', (req, res) => {
-    res.render('./user/regi_success');
+    res.render('./user/regi_success', {
+        sess: sess
+    });
 })
 router.get('/admin/auto_increment_reset', (req, res) => {
-    res.render('./admin/auto_increment_reset');
+    res.render('./admin/auto_increment_reset', {
+        sess: sess
+    });
 })
 router.get('/admin/decrypto_test', (req, res) => {
-    res.render('./admin/decrypto_test');
+    res.render('./admin/decrypto_test', {
+        sess: sess
+    });
 })
 
 module.exports = router
