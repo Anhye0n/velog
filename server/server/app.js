af0ef073180067cfea3e018164ad363c1160c587
anhye0n <anhye0ndev@gmail.com>
2021-06-23 09:51:19 +0900

    test

--- server/server/app.js
+++ server/server/app.js
@@ -27,13 +27,13 @@ app.use(session({
     store: new mysqlStore(db_info.db_info)
 }))
 
-//views 파일
-const view_router = require('./routes/view_ejs')
-app.use('/', view_router)
-
 //라우터
 const user_info = require('./routes/user')
 app.use('/api', user_info)
+
+//views 파일
+const view_router = require('./routes/view_ejs')
+app.use('/', view_router)
 
 const db_test = require('./routes/admin')
 app.use('/admin', db_test)
