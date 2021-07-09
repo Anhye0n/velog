cb820c3626dea4c1598e7830ab2b35eeacc660fc
anhye0n <anhye0ndev@gmail.com>
2021-07-09 09:51:16 +0900

    router test

--- server/server/routes/view_ejs.js
+++ server/server/routes/view_ejs.js
@@ -14,4 +14,49 @@ router.get('/admin/decrypto_test', (req, res) => {
     res.render('./admin/decrypto_test');
 })
 
+router.post('/user/login', passport.authenticate('local-login', {
+    successRedirect: '/api/user/login_success',
+    failureRedirect: '/api/user/login',
+    failureFlash: true
+}), function (req, res) {
+    req.session.save(function () {
+        console.log('session save..')
+        res.redirect('/api/user/login_success')
+    })
+})
+
+router.get('/user/logout', function (req, res) {
+    req.logout();
+    req.session.save((err) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
+})
+
+router.get('/user/login_success', (req, res) => {
+    res.render('./user/login_success', {'user': req.user});
+})
+
+router.get('/user/register', (req, res) => {
+    res.render('./user/register');
+})
+
+router.get('/user/login', (req, res) => {
+    let msg;
+    let err = req.flash('error')
+    if (err) {
+        msg = err;
+    }
+    let user;
+    let user_info = req.user
+    if (user_info) {
+        user = user_info;
+    }
+    res.render('./user/login',{'errMsg': msg, 'user': user});
+})
+
+router.get('/user/regi_success', (req, res) => {
+
+    res.render('./user/regi_success');
+})
 module.exports = router
