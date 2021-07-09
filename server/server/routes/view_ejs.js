c166e51726eabfce040f566dce081797449e44cf
anhye0n <anhye0ndev@gmail.com>
2021-07-09 16:21:03 +0900

    라우터 설정

--- server/server/routes/view_ejs.js
+++ server/server/routes/view_ejs.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('./user/index');
-})
+module.exports = () => {
+    router.get('/', (req, res) => {
+        res.render('./user/index');
+    })
 
-router.get('/admin/auto_increment_reset', (req, res) => {
+    router.get('/admin/auto_increment_reset', (req, res) => {
 
-    res.render('./admin/auto_increment_reset');
-})
-router.get('/admin/decrypto_test', (req, res) => {
+        res.render('./admin/auto_increment_reset');
+    })
+    router.get('/admin/decrypto_test', (req, res) => {
 
-    res.render('./admin/decrypto_test');
-})
+        res.render('./admin/decrypto_test');
+    })
 
-module.exports = router
+    router.get('/user/login_success', (req, res) => {
+        res.render('./user/login_success', {'user': req.user});
+    })
+
+    router.get('/user/register', (req, res) => {
+        res.render('./user/register');
+    })
+
+    router.get('/user/login', (req, res) => {
+        let msg;
+        let err = req.flash('error')
+        if (err) {
+            msg = err;
+        }
+        let user;
+        let user_info = req.user
+        if (user_info) {
+            user = user_info;
+        }
+        res.render('./user/login', {'errMsg': msg, 'user': user});
+    })
+
+    router.get('/user/regi_success', (req, res) => {
+
+        res.render('./user/regi_success');
+    })
+}
