5e52fbb0e0c43bcd074ecc66a5704c30c9a9f069
anhye0n <anhye0ndev@gmail.com>
2021-07-09 16:14:46 +0900

    라우터 설정

--- server/server/routes/view_ejs.js
+++ server/server/routes/view_ejs.js
@@ -14,39 +14,4 @@ router.get('/admin/decrypto_test', (req, res) => {
     res.render('./admin/decrypto_test');
 })
 
-router.get('/user/logout', function (req, res) {
-    req.logout();
-    req.session.save((err) => {
-        if (err) throw err;
-        res.redirect('/');
-    });
-})
-
-router.get('/user/login_success', (req, res) => {
-    res.render('./user/login_success', {'user': req.user});
-})
-
-router.get('/user/register', (req, res) => {
-    res.render('./user/register');
-})
-
-router.get('/user/login', (req, res) => {
-    let msg;
-    let err = req.flash('error')
-    if (err) {
-        msg = err;
-    }
-    let user;
-    let user_info = req.user
-    if (user_info) {
-        user = user_info;
-    }
-    res.render('./user/login', {'errMsg': msg, 'user': user});
-})
-
-router.get('/user/regi_success', (req, res) => {
-
-    res.render('./user/regi_success');
-})
-
 module.exports = router
