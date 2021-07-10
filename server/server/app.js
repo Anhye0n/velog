a5e82565e2526b2b45df121fbafd41fa6f542fdc
anhye0n <anhye0ndev@gmail.com>
2021-07-10 10:58:15 +0900

    라우터 설정 / 오타 수정

--- server/server/app.js
+++ server/server/app.js
@@ -26,6 +26,8 @@ app.use(session({
     cookie:{maxAge:3.6e+6} // 1시간
 }))
 
+require('./routes/user/passport')(passport, router)
+
 app.use(passport.initialize()) //passport를 사용하도록 설정
 app.use(passport.session()) // user 사용 시 session을 활용
 app.use(flash())
@@ -35,7 +37,7 @@ app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'ejs') //ejs 사용
 
 //라우터
-const user_info = require('./routes/user/passport')(passport, router)
+const user_info = require('./routes/user/register')
 app.use('/api', user_info)
 
 //views 파일
