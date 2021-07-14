68916552b05553e6ca8cae23acc6925dc1f29990
anhye0n <anhye0ndev@gmail.com>
2021-07-14 09:27:10 +0900

    admin router Modify

--- server/server/routes/admin/admin_view.js
+++ server/server/routes/admin/admin_view.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/admin', (req, res) => {
+router.get('/', (req, res) => {
     let user = req.user
     res.render('./admin/index', {'user': user});
 })
 
-router.get('/admin/auto_increment_reset', (req, res) => {
+router.get('/auto_increment_reset', (req, res) => {
     let user = req.user
 
     res.render('./admin/auto_increment_reset', {'user': user});
 })
-router.get('/admin/decrypto_test', (req, res) => {
+router.get('/decrypto_test', (req, res) => {
     let user = req.user
 
     res.render('./admin/decrypto_test', {'user': user});
