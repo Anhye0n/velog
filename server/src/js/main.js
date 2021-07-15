02d60778142b07ced0d25b3f5b07216d12485175
anhye0n <anhye0ndev@gmail.com>
2021-07-15 15:54:59 +0900

    img location

--- server/src/js/main.js
+++ server/src/js/main.js
@@ -1,7 +1,7 @@
 const contents_title = document.getElementsByClassName('article_box_title')
 const contents_text = document.getElementsByClassName('article_box_content')
 
-document.getElementById('left_top_title').setAttribute('onclick', location.href='http://anhye0n.me/')
+document.getElementById('left_top_title').setAttribute('onclick', "location.href='http://anhye0n.me/'")
 
 const contents_title_length = contents_title.length
 const contents_text_length = contents_text.length
