612b4c0749a60ca983ab090f6bf910b52bdd89e2
anhye0n <anhye0ndev@gmail.com>
2021-07-15 15:49:25 +0900

    all_article, all_categories Edit

---
+++ server/src/js/main.js
@@ -0,0 +1,23 @@
+const contents_title = document.getElementsByClassName('article_box_title')
+const contents_text = document.getElementsByClassName('article_box_content')
+
+const contents_title_length = contents_title.length
+const contents_text_length = contents_text.length
+
+for (i = 0; i < contents_title_length; i++) {
+    const contents_title_contents = contents_title[i].innerText
+    const contents_title_contents_length = contents_title_contents.length
+
+    if (contents_title_contents_length > 11) {
+        contents_title[i].innerText = contents_title_contents.substr(0, 11) + ' ...'
+    }
+}
+for (i = 0; i < contents_text_length; i++) {
+    const contents_text_contents = contents_text[i].innerText
+    const contents_text_contents_length = contents_text_contents.length
+
+    if (contents_text_contents_length > 153) {
+        contents_text[i].innerText = contents_text_contents.substr(0, 153) + ' ...'
+    }
+}
+
